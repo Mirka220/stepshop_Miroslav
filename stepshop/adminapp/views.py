9efdaee4259238c1b234f6f258b9738fe3a32280
Mirka220 <mirka.gamer@mail.ru>
2021-10-06 17:21:57 +0600

    homework_16

--- stepshop/adminapp/views.py
+++ stepshop/adminapp/views.py
@@ -150,6 +150,7 @@ class CategoryListView(ListView):
     model = ProductCategory
     template_name = 'adminapp/categories.html'
     context_object_name = 'objects'
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
